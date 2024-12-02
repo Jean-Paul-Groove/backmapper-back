d5018b536af1f40b4d1d21fd36a37f75af09ea49
Quentin <rousseltin@gmail.com>
2024-12-02 17:46:54 +0100

    add console.log for debug

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,23 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
+  console.log({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT as number,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ['Trip', 'Step', 'User'],
+    synchronize: true,
+  });
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'step-pictures'), {
     prefix: '/api/step-pictures/',
   });
   console.log(process.env.PORT);
+  console.log(join(__dirname, '..', 'step-pictures'));
   await app.listen(process.env.PORT);
 }
 bootstrap();
